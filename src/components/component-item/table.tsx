e8fb197637622e6f1035b4180214a2d66913e069
xiaotian <wanxiaotian888@gmail.com>
2023-03-05 23:05:06 +0800

    feat: 加上排序图表

--- src/components/component-item/table.tsx
+++ src/components/component-item/table.tsx
@@ -1,6 +1,7 @@
 import {ChartDataResponse} from "../../apis/typing";
 import {FixedSizeList} from "react-window"
 import {useEffect, useRef, useState} from "react";
+import {ArrowDownOutlined, ArrowUpOutlined} from "@ant-design/icons";
 
 type TableProps = {
   data: ChartDataResponse | undefined
@@ -16,13 +17,36 @@ export default ({data}: TableProps) => {
     }
   }, [listRef.current])
 
+  const [sortInfo, setSortInfo] = useState<{field: string, sort: "desc" | "asc"}>()
+  function handleSort(headName: string) {
+    if (sortInfo?.field === headName && sortInfo.sort === "asc") {
+      setSortInfo({
+        ...sortInfo,
+        sort: "desc"
+      })
+    } else if (sortInfo?.field === headName && sortInfo.sort === "desc") {
+      setSortInfo(undefined)
+    } else {
+      setSortInfo({
+        field: headName,
+        sort: "asc"
+      })
+    }
+  }
+
   return (
     <div className="flex-grow flex flex-col">
       <div style={{display: "grid", gridTemplateColumns: `repeat(${data?.headers?.length}, 1fr)`}}
            className="border-y border-y-slate-200"
       >
         {data?.headers?.map(h => (
-          <span key={h} className="p-2 border-r border-r-slate-200">{h}</span>
+          <div key={h} className="cursor-pointer flex items-center p-2 border-r border-r-slate-200"
+               onClick={() => handleSort(h)}
+          >
+            {sortInfo?.field === h && sortInfo.sort === "asc" && <ArrowUpOutlined className="!text-gray-400"/>}
+            {sortInfo?.field === h && sortInfo.sort === "desc" && <ArrowDownOutlined className="!text-gray-400"/>}
+            <span className="inline-block ml-1">{h}</span>
+          </div>
         ))}
       </div>
       <FixedSizeList className="flex-grow overflow-auto !h-0"
