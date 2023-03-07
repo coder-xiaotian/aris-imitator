a5bc3583dcc4d26f000db01240247a5ba76d0f66
xiaotian <wanxiaotian888@gmail.com>
2023-03-07 23:55:04 +0800

    refact: 修改图表渲染逻辑，解决切换不同类型时的渲染性能问题

--- src/components/component-item/index.tsx
+++ src/components/component-item/index.tsx
@@ -7,7 +7,8 @@ import {
   ComponentType,
   FilterInfo
 } from "../../apis/typing";
-import {memo, useEffect, useRef} from 'react'
+import {memo, useEffect, useRef, useState} from 'react'
+import type {ReactElement} from 'react'
 import {getColData} from "@/components/component-config-drawer/utils";
 import {useDebounceFn, useRequest} from "ahooks";
 import {Spin} from "antd";
@@ -28,66 +29,72 @@ type ComponentItemProps = {
 }
 export default memo(({componentConfig, aliasMap, metaData, addingFilter, filterList, onSelectFilter}: ComponentItemProps) => {
   // 获取图表的数据
-  const {data, loading, error, run: requstData} = useRequest((fList = []) => request.post<any, ChartDataResponse, ComponentRequestInfo>('/api/dataSets/my_test/query/simple',
-    {
-    considerDistinct: false,
-    includeNullValues: componentConfig.requestState.includeNullValues,
-    size: componentConfig.type === ChartType.PIE ? 50 :
-      componentConfig.type === ChartType.GRID ? 10000
-        : 4000,
-    sortCriteria: [],
-    filterList: {
-      type: 'FilterList',
-      filters: fList,
-      mode: 'AND'
-    },
-    selections: [...componentConfig.requestState.dimensions?.map((d, i) => {
-      const colData = getColData(d.alias, aliasMap, metaData)
-      const selection: any = {
-        type: componentConfig.type !== ChartType.DIST && componentConfig.type !== ChartType.TIME ? 'Standard' as const : 'Distribution' as const,
-        key: colData!.key,
-        asColumn: `x${i}`,
-      }
-      if (componentConfig.type === ChartType.TIME) {
-        selection.config = {
-          type: "timeDistributionConfig",
-          interval: componentConfig.requestState.options?.bucketInterval,
-          timeUnit: componentConfig.requestState.options?.timeUnit
+  const {data, loading, error, run: requstData} = useRequest((fList = []) => {
+    function handleMeasureParams() {
+      const measures = componentConfig.type === ChartType.PIE ? componentConfig.requestState.measureConfigs?.slice(0, 1) : componentConfig.requestState.measureConfigs
+      return measures?.map((m, i) => {
+        const colData = getColData(m.alias, aliasMap, metaData)
+        const type = /*chartConfig.type !== ChartType.DIST && chartConfig.type !== ChartType.TIME ?*/
+          m.aggregation === 'countDistinct' && m.granularities && m.granularities !== 'noTransformation' ? 'CountDistinctDate' as const : 'Agg' as const
+        // : 'Distribution' as const
+
+        return {
+          type,
+          key: colData!.key,
+          asColumn: `y${i}`,
+          aggregation: m.aggregation,
+          granularities: m.granularities !== 'noTransformation' ? m.granularities : undefined
         }
-      } else if (componentConfig.type === ChartType.DIST) {
-        if (componentConfig.requestState.options!.type === 1) {
-          selection.config = {
-            interval: calcBucketIntervalVal(componentConfig.requestState.options?.bucketIntervalUnit || componentConfig.requestState.options?.timeUnit,
-              componentConfig.requestState.options?.bucketInterval ?? 0, "multiply"),
-            type: "distributionConfig"
+      }) ?? []
+    }
+    return request.post<any, ChartDataResponse, ComponentRequestInfo>('/api/dataSets/my_test/query/simple',
+      {
+        considerDistinct: false,
+        includeNullValues: componentConfig.requestState.includeNullValues,
+        size: componentConfig.type === ChartType.PIE ? 50 :
+          componentConfig.type === ChartType.GRID ? 10000
+            : 4000,
+        sortCriteria: [],
+        filterList: {
+          type: 'FilterList',
+          filters: fList,
+          mode: 'AND'
+        },
+        selections: [...componentConfig.requestState.dimensions?.map((d, i) => {
+          const colData = getColData(d.alias, aliasMap, metaData)
+          const selection: any = {
+            type: componentConfig.type !== ChartType.DIST && componentConfig.type !== ChartType.TIME ? 'Standard' as const : 'Distribution' as const,
+            key: colData!.key,
+            asColumn: `x${i}`,
           }
-        } else {
-          selection.numBuckets = componentConfig.requestState.options!.numberOfBuckets! + (componentConfig.requestState.options?.otherBucketPercentage ? 1 : 0)
-          selection.otherBucketPercentage = componentConfig.requestState.options?.otherBucketPercentage
-          selection.type = "AutoDistribution"
-        }
-      }
+          if (componentConfig.type === ChartType.TIME) {
+            selection.config = {
+              type: "timeDistributionConfig",
+              interval: componentConfig.requestState.options?.bucketInterval,
+              timeUnit: componentConfig.requestState.options?.timeUnit
+            }
+          } else if (componentConfig.type === ChartType.DIST) {
+            if (componentConfig.requestState.options!.type === 1) {
+              selection.config = {
+                interval: calcBucketIntervalVal(componentConfig.requestState.options?.bucketIntervalUnit || componentConfig.requestState.options?.timeUnit,
+                  componentConfig.requestState.options?.bucketInterval ?? 0, "multiply"),
+                type: "distributionConfig"
+              }
+            } else {
+              selection.numBuckets = componentConfig.requestState.options!.numberOfBuckets! + (componentConfig.requestState.options?.otherBucketPercentage ? 1 : 0)
+              selection.otherBucketPercentage = componentConfig.requestState.options?.otherBucketPercentage
+              selection.type = "AutoDistribution"
+            }
+          }
 
-      return selection
-    }) ?? [], ...componentConfig.requestState.measureConfigs?.map((m, i) => {
-      const colData = getColData(m.alias, aliasMap, metaData)
-      const type = /*chartConfig.type !== ChartType.DIST && chartConfig.type !== ChartType.TIME ?*/
-        m.aggregation === 'countDistinct' && m.granularities && m.granularities !== 'noTransformation' ? 'CountDistinctDate' as const : 'Agg' as const
-          // : 'Distribution' as const
-
-      return {
-        type,
-        key: colData!.key,
-        asColumn: `y${i}`,
-        aggregation: m.aggregation,
-        granularities: m.granularities !== 'noTransformation' ? m.granularities : undefined
-      }
-    }) ?? []]
-  })
-    .catch(e => {
-      return Promise.reject(e.response.data)
-    }), {
-    refreshDeps: [componentConfig.requestState],
+          return selection
+        }) ?? [], ...handleMeasureParams()]
+      })
+      .catch(e => {
+        return Promise.reject(e.response.data)
+      })
+  }, {
+    refreshDeps: [componentConfig.requestState, componentConfig.type],
     debounceWait: 500,
   })
   useEffect(() => { // 之所以单独对filterList做监听，是为了能够对其判空，为空且正在添加临时过滤器则不请求接口
@@ -96,25 +103,30 @@ export default memo(({componentConfig, aliasMap, metaData, addingFilter, filterL
     requstData(filterList)
   }, [filterList, addingFilter])
 
-  const comMap = {
-    [ComponentType.CHART as string]: componentConfig.type === "grid" ?
-                                        <Table
-                                          data={data as any}
-                                          aliasMap={aliasMap}
-                                          metaData={metaData}
-                                          componentConfig={componentConfig}
-                                        /> :
-                                            <Chart
-                                              aliasMap={aliasMap}
-                                              metaData={metaData}
-                                              data={data}
-                                              componentConfig={componentConfig}
-                                              chartType={componentConfig.type}
-                                              isInverted={componentConfig.viewState.isInverted}
-                                              onSelect={onSelectFilter}
-                                            />
-  }
-  const RenderCom = comMap[componentConfig.configType]
+  const [renderCom, setRenderCom] = useState<ReactElement>()
+  useEffect(() => {
+    if (loading) return
+
+    const comMap = {
+      [ComponentType.CHART as string]: componentConfig.type === "grid" ?
+        <Table
+          data={data as any}
+          aliasMap={aliasMap}
+          metaData={metaData}
+          componentConfig={componentConfig}
+        /> :
+        <Chart
+          aliasMap={aliasMap}
+          metaData={metaData}
+          data={data}
+          componentConfig={componentConfig}
+          chartType={componentConfig.type}
+          isInverted={componentConfig.viewState.isInverted}
+          onSelect={onSelectFilter}
+        />
+    }
+    setRenderCom(comMap[componentConfig.configType])
+  }, [loading])
   const comRef = useRef<{resize: () => void, clearSelection?: () => void}>()
   const {run: handleResize} = useDebounceFn(() => {
     comRef.current?.resize()
@@ -130,7 +142,7 @@ export default memo(({componentConfig, aliasMap, metaData, addingFilter, filterL
         {error ? (
           // @ts-ignore
           <div>{error.messageChain}</div>
-        ) : <RenderCom.type {...RenderCom.props} ref={comRef} />}
+        ) : renderCom && <renderCom.type {...renderCom.props} ref={comRef} />}
       </Spin>
     </ResizeObserver>
   )
